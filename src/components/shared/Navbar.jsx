f071a9b9c8c7531a177ce0206f4d797623d3828e
xxbadgame <yannis.nzuepro@gmail.com>
2025-01-25 18:12:54 +0100

    portfolio

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,12 +2,35 @@ import React from "react";
 
 const Navbar = () => {
 
-    const sections = ['ABOUT', 'PROJECTS', 'SERVICES', 'PROCESS', 'FAQ']
+    const navItems = ['ABOUT', 'PROJECTS', 'SERVICES', 'PROCESS', 'FAQ']
 
     return (
-        <ul>
-            {sections.map((item, index) => (
-                <li key={index}>{item}</li>
+        <ul style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            width: "5vw",
+            padding: "0",
+            margin: "0",
+            listStyle: "none",
+        }}>
+            {navItems.map((item, index) => (
+                <li 
+                    key={index}
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        transform: "rotate(-90deg)",
+                        transformOrigin: "center",
+                        cursor: "pointer",
+                        marginBlock:"50px",
+                    }}
+                >
+                        {item}
+                </li>
             ))}
         </ul>
     );
