79702990b7f28e8ddc9c496684fc68789dc7c960
xxbadgame <yannis.nzuepro@gmail.com>
2025-02-21 00:20:55 +0100

    pf2

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -4,12 +4,12 @@ import Clock from "../my-ui/clock";
 
 const Contact = () => {   
     return (
-        <div className="bg-white flex flex-row justify-between h-[100vh] pb-16 md:pb-0" id="contact">
-            <div className="md:w-[35%] w-[60%] text-left p-12 md:p-16 flex flex-col justify-between">
-                <p className="text-neutral-400 font-mono">GO TO THE TOP</p>
+        <div className="bg-white flex flex-col md:flex-row justify-between md:h-[100vh] pb-16 md:pb-0" id="contact">
+            <div className="md:w-[35%] w-[100%] md:text-left p-8 md:p-16 flex flex-col justify-between border-b border-gray-200">
+                <p className="text-neutral-400 font-mono text-left">GO TO TOP</p>
                 <div className="flex flex-col">
-                    <h2 className="text-neutral-800 text-3xl md:text-[3vw] leading-none">Let's Talk About <br/> Your Vision</h2>
-                    <div className="flex md:flex-row flex-col mt-32">
+                    <h2 className="text-neutral-800 text-5xl md:text-[3vw] leading-[60px] mt-8">Let's Talk About <br/> Your Vision</h2>
+                    <div className="flex justify-between mt-16 md:mt-32 text-left">
                         <div>
                             <p className="text-neutral-400">PHONE NUMBER</p>
                             <p className="text-neutral-800 font-bold">06 09 63 05 23</p>
@@ -20,7 +20,7 @@ const Contact = () => {
                             <p className="text-neutral-400">CURRENT TIME</p>
                             <p className="text-neutral-800 font-bold"><Clock/></p>
                         </div>
-                        <div className="md:ml-12">
+                        <div className="md:ml-12 text-right">
                             <p className="text-neutral-400">SOCIALS</p>
                             <div className="flex flex-col">
                                 <a className="font-mono text-neutral-800" href="https://www.linkedin.com/in/yannis-nzue-essono-0089571a6/">Linkedin</a>
@@ -30,22 +30,22 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <div className="text-neutral-800">
-                    <p className="text-[]">Yannis Nzue Essono</p>
+                <div className="text-neutral-800 mt-16 md:mt-0">
+                    <p className="text-2xl font-bold">Yannis Nzue Essono</p>
                     <p>FULL-STACK DEVELOPER</p>
                 </div>
             </div>
-            <div className="relative flex size-full items-center justify-center overflow-hidden h-[100vh] w-0 lg:w-[50%]">
+            <div className="relative flex size-full items-center justify-center overflow-hidden md:h-[100vh] w-0 lg:w-[50%]">
                 <Globe className="top-28"/>
             </div>
-            <div className="w-[40%] lg:w-[15%] p-8 md:border-l border-neutral-200 flex flex-col justify-between">
-                <div className="text-neutral-400 text-sm font-mono text-left">
+            <div className="w-[100%] md:w-[40%] lg:w-[15%] p-8 md:border-l border-neutral-200 flex flex-col-reverse md:flex-col justify-between">
+                <div className="text-neutral-400 text-sm font-mono md:text-left pt-8 md:pt-0">
                     <p>MADE BY YANNIS</p>
                     <p>CREATE IN FEBRUARY 2025</p>
                     <p>MADE WITH LOVE AND REACT</p>
                     <p>@ All rights reserved</p>
                 </div>
-                <div>
+                <div className="flex md:flex-col justify-between border-b border-gray-200 pb-16">
                     <p className="text-neutral-400 text-sm font-mono text-left">SITE MAP</p>
                     <p className="text-neutral-400 text-sm font-mono text-left">PROJECTS</p>
                 </div>
